77e1f7688a56f9a7955d1a34c5c616634abe60ba
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-02-28 08:16:29 +0300

    Connection between database and view improved by using ProductModel in selection. List page now can view a specific category's products.

--- E-Commerce.MvcWebUI/Controllers/HomeController.cs
+++ E-Commerce.MvcWebUI/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using E_Commerce.MvcWebUI.Entity;
+using E_Commerce.MvcWebUI.Models;
 
 namespace E_Commerce.MvcWebUI.Controllers
 {
@@ -14,7 +15,18 @@ namespace E_Commerce.MvcWebUI.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View(_context.Products.Where(i=>i.IsHome && i.IsApproved).ToList());
+            return View(_context.Products
+                .Where(i => i.IsHome && i.IsApproved)
+                .Select(i => new ProductModel()
+                {
+                    Id = i.Id,
+                    Name = i.Name.Length > 90 ? i.Name.Substring(0, 87) + "..." : i.Name,
+                    Description = i.Description.Length > 200 ? i.Description.Substring(0, 197) + "..." : i.Description,
+                    Price = i.Price,
+                    Stock = i.Stock,
+                    Image = i.Image,
+                    CategoryId = i.CategoryId
+                }).ToList());
         }
 
         public ActionResult Details(int id)
@@ -22,9 +34,32 @@ namespace E_Commerce.MvcWebUI.Controllers
             return View(_context.Products.FirstOrDefault(i => i.Id == id));
         }
 
-        public ActionResult List()
+        public ActionResult List(int? id)
         {
-            return View(_context.Products.Where(i => i.IsApproved).ToList());
+            var products = _context.Products
+                .Where(i => i.IsApproved)
+                .Select(i => new ProductModel()
+                {
+                    Id = i.Id,
+                    Name = i.Name.Length > 100 ? i.Name.Substring(0, 97) + "..." : i.Name,
+                    Description = i.Description.Length > 200 ? i.Description.Substring(0, 197) + "..." : i.Description,
+                    Price = i.Price,
+                    Stock = i.Stock,
+                    Image = i.Image,
+                    CategoryId = i.CategoryId
+                }).AsQueryable();
+
+            if (id!=null)
+            {
+                products = products.Where(i => i.CategoryId == id);
+            }
+
+            return View(products.ToList());
+        }
+
+        public PartialViewResult _GetCategories()
+        {
+            return PartialView(_context.Categories.ToList());
         }
     }
 }
