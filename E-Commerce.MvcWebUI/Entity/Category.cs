d65bb5ab59bfd6b74e2c97a63e877d323514d514
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-02-29 18:17:25 +0300

    Annotations added.

--- E-Commerce.MvcWebUI/Entity/Category.cs
+++ E-Commerce.MvcWebUI/Entity/Category.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +11,10 @@ namespace E_Commerce.MvcWebUI.Entity
     {
         public int Id { get; set; }
 
+        [DisplayName("Category Name")]
+        [StringLength(maximumLength:20,ErrorMessage ="Name must be shorter(max 20 character)")]
         public string Name { get; set; }
+        [DisplayName("Category Description")]
         public string Description { get; set; }
 
         public List<Product> Products { get; set; }
