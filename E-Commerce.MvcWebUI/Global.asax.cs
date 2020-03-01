b041c16c1ea135d5761b6919a3ad3a72b430468e
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-01 05:55:25 +0300

    Added initializers.

--- E-Commerce.MvcWebUI/Global.asax.cs
+++ E-Commerce.MvcWebUI/Global.asax.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using E_Commerce.MvcWebUI.Entity;
+using E_Commerce.MvcWebUI.Identity;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +16,8 @@ namespace E_Commerce.MvcWebUI
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Database.SetInitializer(new DataInitializer());
+            Database.SetInitializer(new IdentityInitializer());
         }
     }
 }
