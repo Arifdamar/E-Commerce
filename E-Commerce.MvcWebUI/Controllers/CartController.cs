559e0d75846847762a5cb75a862c5119ae261f54
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-01 19:04:17 +0300

    Added Cart controller and necessary methods.

---
+++ E-Commerce.MvcWebUI/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using E_Commerce.MvcWebUI.Entity;
+using E_Commerce.MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace E_Commerce.MvcWebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View(GetCart());
+        }
+
+        public ActionResult AddToCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().AddProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int Id)
+        {
+            var product = db.Products.FirstOrDefault(i => i.Id == Id);
+
+            if (product != null)
+            {
+                GetCart().DeleteProduct(product);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public Cart GetCart()
+        {
+            // Session is special for every user
+            var cart = (Cart)Session["Cart"];
+
+            // If the user doesn't have a cart, create a new cart and assign the cart to the user
+            if(cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+            return cart;
+        }
+
+        public PartialViewResult CartSummary()
+        {
+            return PartialView(GetCart());
+        }
+    }
+}
