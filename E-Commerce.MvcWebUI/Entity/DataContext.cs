373ebcf5e45c42fdb854e2c4d13d5dff02cdd6ba
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-01 05:55:05 +0300

    Initializer moved to Global.asax.cs

--- E-Commerce.MvcWebUI/Entity/DataContext.cs
+++ E-Commerce.MvcWebUI/Entity/DataContext.cs
@@ -10,7 +10,6 @@ namespace E_Commerce.MvcWebUI.Entity
     {
         public DataContext():base("dataConnection")
         {
-            Database.SetInitializer(new DataInitializer());
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
