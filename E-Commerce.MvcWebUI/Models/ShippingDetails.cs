f314f0a919d2c4c566ee154591bd5491a7f3392d
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-02 22:20:38 +0300

    Add Shipping details.

---
+++ E-Commerce.MvcWebUI/Models/ShippingDetails.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace E_Commerce.MvcWebUI.Models
+{
+    public class ShippingDetails
+    {
+        [Required(ErrorMessage = "Please enter the name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Please enter the title")]
+        public string AddressTitle { get; set; }
+
+        [Required(ErrorMessage = "Please enter the address")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter the city")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please enter the district")]
+        public string District { get; set; }
+
+        [Required(ErrorMessage = "Please enter the street")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Please enter the zip code")]
+        public string ZipCode { get; set; }
+
+
+    }
+}
