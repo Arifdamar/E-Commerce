6d0ec21f16080617cedc31789811357cef797d72
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-02 22:35:19 +0300

    Get and post actions of Checkout are added. Order details saved to database through SaveOrder method.

--- E-Commerce.MvcWebUI/Controllers/CartController.cs
+++ E-Commerce.MvcWebUI/Controllers/CartController.cs
@@ -48,7 +48,7 @@ namespace E_Commerce.MvcWebUI.Controllers
             var cart = (Cart)Session["Cart"];
 
             // If the user doesn't have a cart, create a new cart and assign the cart to the user
-            if(cart == null)
+            if (cart == null)
             {
                 cart = new Cart();
                 Session["Cart"] = cart;
@@ -59,6 +59,68 @@ namespace E_Commerce.MvcWebUI.Controllers
         public PartialViewResult CartSummary()
         {
             return PartialView(GetCart());
+        }
+
+        public ActionResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ActionResult Checkout(ShippingDetails entity)
+        {
+            var cart = GetCart();
+
+            if (cart.cartLines.Count == 0)
+            {
+                ModelState.AddModelError("NoProductError", "There is no products in your cart.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                SaveOrder(cart, entity);
+
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(entity);
+            }
+            
+        }
+
+        private void SaveOrder(Cart cart, ShippingDetails entity)
+        {
+            var order = new Order();
+
+            order.OrderNumber = "O" + (new Random()).Next(11111, 99999).ToString();
+            order.Total = cart.TotalPrice();
+            order.OrderDate = DateTime.Now;
+
+            order.UserName = entity.UserName;
+            order.AddressTitle = entity.AddressTitle;
+            order.Address = entity.Address;
+            order.City = entity.City;
+            order.District = entity.District;
+            order.Street = entity.Street;
+            order.ZipCode = entity.ZipCode;
+
+            order.OrderLines = new List<OrderLine>();
+
+            foreach (var line in cart.cartLines)
+            {
+                OrderLine orderLine = new OrderLine();
+                orderLine.Quantity = line.Quantity;
+                orderLine.Price = line.Quantity * line.Product.Price;
+                orderLine.ProductId = line.Product.Id;
+
+                order.OrderLines.Add(orderLine);
+                line.Product.Stock -= line.Quantity;
+            }
+            db.Orders.Add(order);
+            db.SaveChanges();
+
         }
     }
 }
