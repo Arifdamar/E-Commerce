0c44aeafed18caf0f8741fed931b8c7c8cc6b990
Arif Damar <38383027+Arifdamar@users.noreply.github.com>
2020-03-03 05:31:17 +0300

    Users's necessary datas to use in Account/Index and Account/Details is selected from database.

--- E-Commerce.MvcWebUI/Controllers/AccountController.cs
+++ E-Commerce.MvcWebUI/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using E_Commerce.MvcWebUI.Identity;
+﻿using E_Commerce.MvcWebUI.Entity;
+using E_Commerce.MvcWebUI.Identity;
 using E_Commerce.MvcWebUI.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
@@ -16,6 +17,8 @@ namespace E_Commerce.MvcWebUI.Controllers
         private UserManager<ApplicationUser> UserManager;
         private RoleManager<ApplicationRole> RoleManager;
 
+        private DataContext db = new DataContext();
+
         public AccountController()
         {
             var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
@@ -23,6 +26,50 @@ namespace E_Commerce.MvcWebUI.Controllers
 
             var roleStore = new RoleStore<ApplicationRole>(new IdentityDataContext());
             RoleManager = new RoleManager<ApplicationRole>(roleStore);
+        }
+
+        [Authorize]
+        public ActionResult Index()
+        {
+            var orders = db.Orders.Where(i => i.UserName == User.Identity.Name).Select(i => new UserOrderModel()
+            {
+                Id = i.Id,
+                OrderDate = i.OrderDate,
+                OrderNumber = i.OrderNumber,
+                OrderState = i.OrderState,
+                Total = i.Total
+            }).OrderByDescending(i => i.OrderDate).ToList();
+
+            return View(orders);
+        }
+
+        [Authorize]
+        public ActionResult Details(int id)
+        {
+            var entity = db.Orders.Where(i => i.Id == id).Select(i => new OrderDetailsModel()
+            {
+                OrderId = i.Id,
+                OrderNumber = i.OrderNumber,
+                Total = i.Total,
+                OrderDate = i.OrderDate,
+                OrderState = i.OrderState,
+                AddressTitle = i.AddressTitle,
+                Address = i.Address,
+                City = i.City,
+                District = i.District,
+                Street = i.Street,
+                ZipCode = i.ZipCode,
+                OrderLines = i.OrderLines.Select(j => new OrderLineModel()
+                {
+                    ProductId = j.ProductId,
+                    ProductName = j.Product.Name.Length>75?j.Product.Name.Substring(0,72)+"...":j.Product.Name,
+                    Price = j.Price,
+                    Image = j.Product.Image,
+                    Quantity = j.Quantity
+                }).ToList()
+            }).FirstOrDefault();
+
+            return View(entity);
         }
 
         // GET: Account
@@ -69,13 +116,13 @@ namespace E_Commerce.MvcWebUI.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Login(Login model,string ReturnUrl)
+        public ActionResult Login(Login model, string ReturnUrl)
         {
             if (ModelState.IsValid)
             {
                 var user = UserManager.Find(model.UserName, model.Password);
 
-                if(user != null)
+                if (user != null)
                 {
                     var authManager = HttpContext.GetOwinContext().Authentication;
                     var identityClaims = UserManager.CreateIdentity(user, "ApplicationCookie");
@@ -106,7 +153,7 @@ namespace E_Commerce.MvcWebUI.Controllers
             var authManager = HttpContext.GetOwinContext().Authentication;
             authManager.SignOut();
 
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
 
     }
